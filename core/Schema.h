777311551eecf9a737ce33b879c8a7bff3ff91e7
kicsy <lmj07luck@126.com>
2018-11-01 19:27:12 +0800

    ...

--- core/Schema.h
+++ core/Schema.h
@@ -2,10 +2,11 @@
 #include <cassert>
 #include "Pin.h"
 #include "Statement.h"
+#include "DataPack.h"
 #include "DataAdapter.h"
 namespace fs
 {
-	template<typename ProcessType /*= void(Context&, DataPack&)*/>
+	template<typename ProcessType>
 	class Schema : public DataAdapter, public std::enable_shared_from_this<Schema<ProcessType>>
 	{
 	public:
@@ -20,29 +21,7 @@ namespace fs
 			:std::enable_shared_from_this<Schema<ProcessType>>()
 			,_transformFunc(transformFunc)
 			,_toDataFunc(toDataFunc)
-			,_toAnyValuesFunc(toAnyValuesFunc)
-		{
-			//if (!_transformFunc) _transformFunc = [](InnerProcessType innerProcess, Context& context, const P_Data& pdata) {
-			//	//transform...
-			//	DataPack* pvs = (DataPack*)pdata.get();
-			//	if (innerProcess && pvs)
-			//	{
-			//		innerProcess(context, *pvs);
-			//	}
-			//};
-			//if (!_toDataFunc) _toDataFunc = [](const AnyValues &values) -> P_Data {
-			//	return std::move(std::make_shared<DataPack>(values));
-			//};
-			//if (!_toAnyValuesFunc) _toAnyValuesFunc = [](const P_Data& pdata) ->AnyValues
-			//{
-			//	DataPack* p = (DataPack*)pdata.get();
-			//	if (p)
-			//	{
-			//		return *(AnyValues*)p;
-			//	}
-			//	return std::move(AnyValues());
-			//};
-		}
+			,_toAnyValuesFunc(toAnyValuesFunc){}
 		virtual ~Schema() {}
 
 		virtual void callInnerProcess(InnerProcessType innerProcess, Context& context, const P_Data& pdata)
@@ -89,8 +68,86 @@ namespace fs
 		ToAnyValuesFunc _toAnyValuesFunc;
 	};
 
-	
 	using defaultProcess = void(Context&, DataPack&);
 	std::shared_ptr<Schema< defaultProcess>> defaultSchema();
+
+
+
+	template<typename DataSetType>
+	class LightSchema : public DataAdapter, public std::enable_shared_from_this<LightSchema<DataSetType>>
+	{
+	public:
+		using InnerProcessType = std::function< void(Context&, typename DataSetType&)>;
+		explicit LightSchema():std::enable_shared_from_this<Schema<ProcessType>>()
+		virtual ~LightSchema() {}
+		virtual void callInnerProcess(InnerProcessType innerProcess, Context& context, const P_Data& pdata)
+		{
+			DataSetType* pvs = (DataSetType*)pdata.get();
+			if (innerProcess && pvs)
+			{
+				innerProcess(context, *pvs);
+			}
+		}
+
+		P_Pin addPin(P_Spore &spore, const std::string &name, InnerProcessType process = nullptr)
+		{
+			return addPin(
+				spore,
+				name,
+				process == nullptr ? Pin_Type::OUT_PIN : Pin_Type::IN_PIN,
+				process);
+		}
+
+		P_Pin addPin(P_Spore &spore, const std::string &name, Pin_Type type, InnerProcessType process = nullptr)
+		{
+			auto pin = P_Pin(new WarpPin<Schema<typename ProcessType>>(spore, name, type, shared_from_this(), process));
+			return spore->addPin(pin);
+		}
+
+		virtual bool equal(DataAdapter* other)
+		{
+			return (DataAdapter*)this == other;
+		}
+		virtual P_Data toData(const AnyValues &values)
+		{
+
+			return P_Data((Data*)(toDataFunc(values)));
+		}
+
+		virtual AnyValues toAnyValues(const P_Data& pdata)
+		{
+			DataSetType* p = (DataSetType*)pdata.get();
+			if (p)
+				return toAnyValuesFunc(*p);
+			return std::move(AnyValues());
+		}
+	protected:
+		virtual DataSetType* toDataFunc(const AnyValues &)
+		{
+			return NULL;
+		}
+
+		virtual AnyValues toAnyValuesFunc(const DataSetType &)
+		{
+			return std::move(AnyValues());
+		}
+
+	protected:
+		friend Spore;
+	};
+
+	class DefaultSchema : public LightSchema<DataPack>
+	{
+	protected:
+		virtual DataPack* toDataFunc(const AnyValues & values)
+		{
+			return new DataPack(values);
+		}
+
+		virtual AnyValues toAnyValuesFunc(const DataPack & dataPack)
+		{
+			return *(AnyValues*)(&dataPack);
+		}
+	};
 }
 
